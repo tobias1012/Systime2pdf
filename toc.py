1d44ebe53960f11682e1b12e35b69d40f2ce42ee
Tobias Møller Jensen <tobias1012@gmail.com>
2020-06-03 15:58:48 +0200

    Toc Now works

--- toc.py
+++ toc.py
@@ -1,21 +1,47 @@
+#!/usr/bin/env python3
+
+from fpdf import FPDF
+
 from pdfWriter import pdf
 
 
 class TOC:    
-    def __init__(self):
-        self.entry = dict()
+    def __init__(self, name):
+        self.line = 1
+        self.name = name
+        self.sections = []
     def addEntry(self, name: str):
-        if name not in self.entry.keys():
-            self.entry[name] = []
-
-    def addSubEntry(self, entry: str, subName: str):
-        if entry not in self.entry.keys():
+        if(name in self.sections):
             pass
-        self.entry[entry].append(subName)
-    def createPage(self, pdffile: pdf) -> int:
-        for it in self.entry.keys():
-            for jt in self.entry[it]:
-                pass
+        self.sections.append(TOC(name))
 
+    def _createSection(self, size: float, arr, pdf):
+        arr.append(self.name)
+
+        #add to pdf
+        pdf.set_font("DejaVu", size=size)
+        pdf.cell(200, 10, txt=self.name, ln=self.line, align="C")
+        self.line += 1
+        for it in self.sections:
+            arr.append(it._createSection(size -5, arr, pdf))
+        return arr
+
+
+    def createPage(self, pdf: FPDF): #create a pdf page
+        size = 25
+        arr = []
+        pdf.add_page()
+        text = self._createSection(size, arr, pdf)
+        
+    def __getitem__(self, id: str):
+        ret = [item for item in self.sections if item.name == id] # Get list of all with the right name
+        return ret[0]
+
+    def __setitem__(self, index: str, obj):
+        self.sections.append(obj)
+    def __str__(self):
+        return self.name
 
                 
+if __name__ == "__main__":
+    print("This shoulnd't be run as main")
